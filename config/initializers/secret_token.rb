40c165859f57efbd4aba4a3cd1f4c4271de1c934
serega <sergey1990@openteam.ru>
2013-01-16 12:28:09 +0700

    Probnii repoz

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '97932b7d07df77a32e6f1a2efbb02afddfcb302404e15e37c3aa3330cedde277cc50b52db6f3bc1f2290b892b2ca4ce926696b1f55bf3eeab9249e278d1eb5ad'
